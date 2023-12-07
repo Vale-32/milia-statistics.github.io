6007379b2356baf46b7aeae496683c1dd0468f44
Vale-32 <valentino.milia@yahoo.com>
2023-12-07 20:18:13 +0100

    Hw 1-2 rivisti

---
+++ Homework1/Csharp/WinForm/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class DrawingForm : Form
+{
+    public DrawingForm()
+    {
+        // Define title of the window
+        this.Text = "Forms Drawing";
+
+        // generate Paint module
+        this.Paint += new PaintEventHandler(OnPaint);
+    }
+
+    private void OnPaint(object sender, PaintEventArgs e)
+    {
+        // create a graphic object from paint graphics evenment
+        Graphics g = e.Graphics;
+
+        // create pen object in order to draw
+        Pen pen = new Pen(Color.Black);
+
+        // draw circle
+        g.DrawEllipse(pen, 50, 50, 100, 100);
+
+        // draw point
+        g.DrawRectangle(pen, 200, 50, 1, 1);
+
+        // draw line
+        g.DrawLine(pen, 50, 200, 150, 200);
+
+        // draw rectangle
+        g.DrawRectangle(pen, 200, 200, 100, 50);
+    }
+
+    public static void Main()
+    {
+        Application.Run(new DrawingForm());
+    }
+}
